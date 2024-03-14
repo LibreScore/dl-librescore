ddab9493ec0b67ddc4373b12d671e0273e138cca
TheAfroOfDoom <tehafroofdoom@gmail.com>
2024-03-14 11:19:33 -0400

    generate api auth token with md5 algorithm - fixes https://github.com/LibreScore/dl-librescore/issues/44 - fixes https://github.com/LibreScore/dl-librescore/issues/61

--- src/file.ts
+++ src/file.ts
@@ -1,9 +1,8 @@
 /* eslint-disable no-extend-native */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 
-import isNodeJs from "detect-node";
+import md5 from "md5";
 import { getFetch } from "./utils";
-import { auths } from "./file-magics";
 
 export type FileType = "img" | "mp3" | "midi";
 
@@ -11,105 +10,10 @@ const getApiUrl = (id: number, type: FileType, index: number): string => {
     return `/api/jmuse?id=${id}&type=${type}&index=${index}`;
 };
 
-let imgInProgress = false;
-
-const getApiAuth = async (type: FileType, index: number): Promise<string> => {
-    if (isNodeJs) {
-        // fix later
-        throw Error;
-    }
-
-    let numPages = 0;
-    let pageCooldown = 25;
-    if (!auths[type + index]) {
-        try {
-            switch (type) {
-                case "midi": {
-                    const fsBtn = document.querySelector(
-                        'button[title="Toggle Fullscreen"]'
-                    ) as HTMLButtonElement;
-                    if (!fsBtn) {
-                        throw Error;
-                    }
-                    const el =
-                        fsBtn.parentElement?.parentElement?.querySelector(
-                            "button"
-                        ) as HTMLButtonElement;
-                    el.click();
-                    break;
-                }
-                case "mp3": {
-                    const el = document.querySelector(
-                        'button[title="Toggle Play"]'
-                    ) as HTMLButtonElement;
-                    el.click();
-                    break;
-                }
-                case "img": {
-                    if (!imgInProgress) {
-                        imgInProgress = true;
-                        let parentDiv = document.querySelector(
-                            "#jmuse-scroller-component"
-                        )!;
-
-                        numPages = parentDiv.children.length - 3;
-                        let i = 0;
-
-                        function scrollToNextChild() {
-                            let childDiv = parentDiv.children[i];
-                            if (childDiv) {
-                                childDiv.scrollIntoView();
-                            }
-
-                            i++;
-
-                            if (i < numPages) {
-                                setTimeout(scrollToNextChild, pageCooldown);
-                            }
-                        }
-
-                        scrollToNextChild();
-                    }
-                    imgInProgress = false;
-                    break;
-                }
-            }
-        } catch (err) {
-            console.error(err);
-            throw Error;
-        }
-    }
-
-    try {
-        return new Promise((resolve, reject) => {
-            let timer = setTimeout(
-                () => {
-                    reject(new Error("token timeout"));
-                },
-                type === "img"
-                    ? numPages * pageCooldown * 2 + 2100
-                    : 5 * 1000 /* 5s */
-            );
-
-            // Check the auths object periodically
-            let interval = setInterval(() => {
-                if (auths.hasOwnProperty(type + index)) {
-                    clearTimeout(timer);
-                    clearInterval(interval);
-                    setInterval(
-                        () => {
-                            resolve(auths[type + index]);
-                        },
-                        // long delay for images to give time for them to load fully
-                        type === "img" ? 2000 : 100
-                    );
-                }
-            }, 100);
-        });
-    } catch {
-        console.error(type, "token timeout");
-        throw Error;
-    }
+const getApiAuth = (id: number, type: FileType, index: number): string => {
+    const suffix = 'et'
+    const code = `${id}${type}${index}${suffix}`;
+    return md5(code).slice(0, 4);
 };
 
 export const getFileUrl = async (
@@ -120,7 +24,7 @@ export const getFileUrl = async (
 ): Promise<string> => {
     let r;
     const url = getApiUrl(id, type, index);
-    const auth = await getApiAuth(type, index);
+    const auth = getApiAuth(id, type, index);
     if (type === "img" && index === 0) {
         // auth is the URL for the first page
         r = await _fetch(auth);
