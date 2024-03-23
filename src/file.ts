eeb069cd91e64089972828192a5cc7ac6b4740ef
Peter Njeim <peterthegreatpeterthegreat@hotmail.com>
2024-03-23 19:16:52 -0300

    fix: update auth token suffix

--- src/file.ts
+++ src/file.ts
@@ -12,7 +12,7 @@ const getApiUrl = (id: number, type: FileType, index: number): string => {
 };
 
 const getApiAuth = (id: number, type: FileType, index: number): string => {
-    const suffix = "%%%@";
+    const suffix = "7%0?7a!";
     const code = `${id}${type}${index}${suffix}`;
     return md5(code).slice(0, 4);
 };
